f76f21d34a7fe4693145949a8d65f7f62b84604f
MadimetjaMadix <madimetjasethosa@gmail.com>
2020-05-08 18:06:12 +0200

    Lab 3, 2.1: Creating a router

---
+++ classRoutes.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const router = express.Router()
+const classList = [] // our class list array
+
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'index.html'))
+})
+
+router.get('/create', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'create.html'))
+})
+
+router.get('/delete', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'delete.html'))
+})
+
+router.post('/edit', function (req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'class', 'edit.html'))
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const mainRouter = require('./mainRouter')
 
-app.use(mainRouter)
+// loading our routers
+const mainRouter = require('./mainRouter')
+const classRouter = require('./classRoutes.js')
+
+// mounting our routers
+app.use('/', mainRouter)
+app.use('/class', classRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port)
