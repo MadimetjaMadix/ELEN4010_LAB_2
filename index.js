cbb27ed8a21706a760b14254719944219691411a
MadimetjaMadix <madimetjasethosa@gmail.com>
2020-05-08 18:16:19 +0200

    Lab 3, 2.2 RESTful Interface

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-const path = require('path')
 const express = require('express')
 const app = express()
 
+// loading body-parser
+const bodyParser = require('body-parser')
+
 // loading our routers
 const mainRouter = require('./mainRouter')
-const classRouter = require('./classRoutes.js')
+const todoRouter = require('./classRoutes.js')
+
+// tell express to use bodyParser for JSON and URL encoded form bodies
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // mounting our routers
 app.use('/', mainRouter)
-app.use('/class', classRouter)
+app.use('/class', todoRouter)
 
 const port = process.env.PORT || 3000
 app.listen(port)
